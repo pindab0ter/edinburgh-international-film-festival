7a222812b4311dc0e10db5d78608c0fcc4aa31a5
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-01-17 17:56:56 +0100

    Remove Floating Action Button from list view

--- app/src/main/java/nl/pindab0ter/edinburghinternationalfilmfestival/FilmListActivity.kt
+++ app/src/main/java/nl/pindab0ter/edinburghinternationalfilmfestival/FilmListActivity.kt
@@ -43,11 +43,6 @@ class FilmListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.title = title
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
         if (film_detail_container != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
