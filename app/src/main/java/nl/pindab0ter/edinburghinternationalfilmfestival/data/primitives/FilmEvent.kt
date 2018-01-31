42283157d5aca067a63632b4bc56bebc315512bd
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-01-31 13:39:15 +0100

    Prune unused FilmEvent properties

--- app/src/main/java/nl/pindab0ter/edinburghinternationalfilmfestival/data/primitives/FilmEvent.kt
+++ app/src/main/java/nl/pindab0ter/edinburghinternationalfilmfestival/data/primitives/FilmEvent.kt
@@ -4,15 +4,8 @@ import com.google.gson.annotations.SerializedName
 import java.util.Date
 
 class FilmEvent {
-    val ageCategory: String? = null
-    val artist: String? = null
-    val artistType: String? = null
     val code: String? = null
-    val country: String? = null
     val description: String? = null
-    val descriptionTeaser: String? = null
-    val festivalId: String? = null
-    val fringeFirst: Boolean? = null
     val genre: String? = null
 
     @SerializedName("genre_tags")
@@ -22,8 +15,6 @@ class FilmEvent {
 
     val latitude: Double? = null
     val longitude: Double? = null
-    val nonEnglish: Boolean? = null
-    val performersNumber: Int? = null
 
     enum class Status {
         @SerializedName("active")
@@ -35,47 +26,11 @@ class FilmEvent {
     }
 
     val status: Status? = null
-    val subTitle: String? = null
-    val subVenue: String? = null
     val title: String? = null
-    val twitter: String? = null
     val updated: Date? = null
     val url: String? = null
-    val warnings: String? = null
     val website: String? = null
     val year: Int? = null
-
-    class Categories {
-        val keywords: Array<String>? = null
-        val strandTitles: Array<String>? = null
-        val subjects: Array<String>? = null
-    }
-
-    val categories: Categories? = null
-
-    class Disabled {
-        val audio: Boolean? = null
-        val audioDates: String? = null
-        val captioning: Boolean? = null
-        val captioningDates: String? = null
-        val otherServices: Boolean? = null
-        val otherServicesDates: String? = null
-        val otherServicesInformation: Boolean? = null
-        val signed: Boolean? = null
-        val signedDates: String? = null
-    }
-
-    val disabled: Disabled? = null
-
-    class Discounts {
-        val friends: Boolean? = null
-        val group: Boolean? = null
-        val passport: Boolean? = null
-        val schools: Boolean? = null
-        val twoForOne: Boolean? = null
-    }
-
-    val discounts: Discounts? = null
 
     open class Images {
 
@@ -124,19 +79,10 @@ class FilmEvent {
 
     val images: Images? = null
 
-    class PerformanceSpace {
-        val ageLimit: String? = null
-        val ageLimited: Boolean? = null
-        val capacity: Int? = null
-        val name: String? = null
-    }
-
-    val performanceSpace: PerformanceSpace? = null
-
     class Performance {
         sealed class Concession {
             class Available(val amount: Int) : Concession()
-            class Unavailable() : Concession()
+            class Unavailable : Concession()
         }
 
         val concession: Concession? = null
@@ -150,42 +96,11 @@ class FilmEvent {
 
     val performances: Array<Performance>? = null
 
-    class UpdateTimes {
-        val ageCategory: Date? = null
-        val artist: Date? = null
-        val description: Date? = null
-        val disabled: Date? = null
-        val discounts: Date? = null
-        val fringeFirst: Date? = null
-        val genre: Date? = null
-        val identity: Date? = null
-        val images: Date? = null
-        val other: Date? = null
-        val performances: Date? = null
-        val status: Date? = null
-        val title: Date? = null
-        val venue: Date? = null
-        val webLinks: Date? = null
-    }
-
-    val updateTimes: UpdateTimes? = null
-
     class Venue {
         val address: String? = null
-        val boxOfficeFringe: String? = null
-        val boxOfficeOpening: String? = null
-        val cafeDescription: String? = null
         val code: String? = null
         val description: String? = null
-        val disabledDescription: String? = null
         val email: String? = null
-        val fax: String? = null
-        val hasBar: Boolean? = null
-        val hasBookingOverCard: Boolean? = null
-        val hasBookingOverPhone: Boolean? = null
-        val hasBookingOverWeb: Boolean? = null
-        val hasCafe: Boolean? = null
-        val mapRef: String? = null
         val name: String? = null
         val phone: String? = null
 
