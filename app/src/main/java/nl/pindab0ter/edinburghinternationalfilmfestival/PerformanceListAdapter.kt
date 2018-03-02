aeb1e37a97f38fede50dfd7ea533e6489580b8ee
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-03-02 16:32:40 +0100

    Update UI to reflect scheduled status

--- app/src/main/java/nl/pindab0ter/edinburghinternationalfilmfestival/PerformanceListAdapter.kt
+++ app/src/main/java/nl/pindab0ter/edinburghinternationalfilmfestival/PerformanceListAdapter.kt
@@ -6,19 +6,49 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.performance_list_item.view.*
+import nl.pindab0ter.edinburghinternationalfilmfestival.data.FilmEventDAO
 import nl.pindab0ter.edinburghinternationalfilmfestival.data.primitives.FilmEvent
 import nl.pindab0ter.edinburghinternationalfilmfestival.utilities.formatForDisplay
 
 class PerformanceListAdapter(context: Context, resource: Int, objects: Array<out FilmEvent.Performance>) : ArrayAdapter<FilmEvent.Performance>(context, resource, objects) {
+    private val filmEventDAO = FilmEventDAO(context)
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val performance = getItem(position)
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.performance_list_item, parent, false)
 
         view.performance_list_item_date.text = performance.start?.formatForDisplay()
 
-        if (performance.scheduled == false) view.performance_list_item_button_add.visibility = View.VISIBLE
-        else view.performance_list_item_button_remove.visibility = View.VISIBLE
+        view.performance_list_item_button_add.apply {
+            tag = position
+            visibility = if (performance.scheduled == false) View.VISIBLE else View.GONE
 
+            setOnClickListener { view ->
+                val clickedPerformance = getItem(view.tag as Int)
+                clickedPerformance.scheduled = true
+                filmEventDAO.update(clickedPerformance)
+
+                (view.parent as View).apply {
+                    performance_list_item_button_add.visibility = View.GONE
+                    performance_list_item_button_remove.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        view.performance_list_item_button_remove.apply {
+            tag = position
+            visibility = if (performance.scheduled == true) View.VISIBLE else View.GONE
+            setOnClickListener { view ->
+                val clickedPerformance = getItem(view.tag as Int)
+                clickedPerformance.scheduled = false
+                filmEventDAO.update(clickedPerformance)
+
+                (view.parent as View).apply {
+                    performance_list_item_button_add.visibility = View.VISIBLE
+                    performance_list_item_button_remove.visibility = View.GONE
+                }
+            }
+        }
         return view
     }
 }
